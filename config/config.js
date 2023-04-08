c7c391dc480f92e8038d4b39358c51e30445c200
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2023-04-08 01:54:36 -0300

    UPDATE CONFIG FILE
    - in config.js:
    - uncomment lines to allow for dev environment

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 /* eslint prefer-const: [0] */
 
-// const mongo = require('../secrets/mongo');
-// const auth = require('../secrets/auth');
+const mongo = require('../secrets/mongo');
+const auth = require('../secrets/auth');
 
 let config = {
   connectionString: 'mongodb://localhost/notesdb',
@@ -13,8 +13,8 @@ if (env === 'production') {
   config.connectionString = process.env.MONGO;
   config.mySecret = process.env.SALT;
 }
-// if (env === 'development') {
-//   config.connectionString = mongo.connectionString;
-//   config.mySecret = auth.mySecret;
-// }
+if (env === 'development') {
+  config.connectionString = mongo.connectionString;
+  config.mySecret = auth.mySecret;
+}
 module.exports = config;
