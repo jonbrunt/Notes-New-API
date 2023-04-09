7c1a70eba9c8bfece4d0c028f4cf14e8bb997970
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2023-04-09 16:59:26 -0300

    USER ENTITY
    - in User.js:
    - create User entity
    - base on previous Mongoose schema
    - create one-to-many relation with Note entity

---
+++ Entities/User.js
@@ -0,0 +1,48 @@
+const { EntitySchema } = require('typeorm');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 11;
+
+module.exports = new EntitySchema({
+  name: 'User',
+  tableName: 'users',
+  columns: {
+    id: {
+      type: 'uuid',
+      primary: true,
+      generated: 'uuid',
+    },
+    email: {
+      type: 'varchar',
+      nullable: false,
+      unique: true,
+      transformer: {
+        to: (value) => value.toLowerCase(),
+        from: (value) => value,
+      },
+    },
+    password: {
+      type: 'varchar',
+      nullable: false,
+    },
+  },
+  listeners: {
+    beforeInsert: async (event) => {
+      const user = event.entity;
+      user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+    },
+  },
+  methods: {
+    checkPassword: async (rawPassword) => {
+      return bcrypt.compare(rawPassword, this.password);
+    },
+  },
+  relations: {
+    notes: {
+      target: 'Note',
+      type: 'one-to-many',
+      inverseSide: 'user',
+      onDelete: 'CASCADE'
+    }
+  }
+});
